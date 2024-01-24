33258ce41f396503127cdcee89ad95c2e9eba004
Sttatic <sttatic2001@gmail.com>
2024-01-23 21:08:29 -0300

    Create User Controller and made simple test with it

---
+++ Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace car_sales_catalog.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    [HttpGet(Name = "GetUser")]
+    public ActionResult GetUser()
+    {
+        return Ok("Teste");
+    }
+
+}
